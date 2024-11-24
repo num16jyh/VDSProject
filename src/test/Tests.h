984ed320f7f286cfad38fe88253972714e6310b3
num16jyh <num16jyh@rptu.de>
2024-11-24 20:06:21 +0100

    Reword Manager::createVar() test comments and var names

--- src/test/Tests.h
+++ src/test/Tests.h
@@ -19,11 +19,12 @@ public:
 // Manager::createVar() test
 TEST_F(ManagerTest, CreateVar)
 {
-    const BDD_ID varsToAdd = 100; // Will try adding this many variables.
+    // Will try adding variables up to this ID.
+    const BDD_ID testIDs = 100;
 
     // Start with an ID of 2 since IDs 0 and 1 must already be assigned to the
     // leaf nodes False and True respectively.
-    for (BDD_ID i = 2; i <= varsToAdd; i++)
+    for (BDD_ID i = 2; i <= testIDs; i++)
         EXPECT_EQ(manager.createVar(::std::to_string(i)), i);
 
     // Attempting to add a label that already exists should return its already
@@ -31,7 +32,7 @@ TEST_F(ManagerTest, CreateVar)
     EXPECT_EQ(manager.createVar("False"), 0);
     EXPECT_EQ(manager.createVar("True"), 1);
 
-    for (BDD_ID i = 2; i <= varsToAdd; i++)
+    for (BDD_ID i = 2; i <= testIDs; i++)
         EXPECT_EQ(manager.createVar(::std::to_string(i)), i);
 }
 
